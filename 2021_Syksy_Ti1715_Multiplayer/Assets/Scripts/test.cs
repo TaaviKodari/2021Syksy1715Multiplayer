dd08e5fecc254eec47d4daba579033dea6497342
kodarit11 <taavi.makela@gmail.com>
2021-08-31 18:17:05 +0300

    luotiin projekti

---
+++ 2021_Syksy_Ti1715_Multiplayer/Assets/Scripts/test.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    GameObject a;
+    Transform transform;
+    Vector3 vector3;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
