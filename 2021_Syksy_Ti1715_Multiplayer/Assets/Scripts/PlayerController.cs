43977bd9010c8dce7d10ae5edc9cca7d7522ff33
kodarit11 <taavi.makela@gmail.com>
2021-10-26 18:13:34 +0300

    mouse look valmis ja hyppy done

--- 2021_Syksy_Ti1715_Multiplayer/Assets/Scripts/PlayerController.cs
+++ 2021_Syksy_Ti1715_Multiplayer/Assets/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour
     {
         CheckIfGrounded();
         Move();
+        Jump();
     }
 
     private void CheckIfGrounded()
@@ -60,5 +61,15 @@ public class PlayerController : MonoBehaviour
         }
 
         controller.Move(move * targetSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
